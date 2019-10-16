b569fbf36791f82ec2e1ddfb7e21d869e8e2b795
abdanbarkaath <abdanbarkaath@fissionlabs.in>
2019-10-16 13:02:40 +0530

    added store employee (vue)

---
+++ store-employee/src/store.js
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  actions: {
+    productaction:(store,employee)=>{
+      store.commit('mutateproduct',employee)
+      // console.log("action-"+employee);
+    }
+  },
+  mutations: {
+    mutateproduct:(store,employee)=>{
+      store.employees = employee
+      // console.log("mutate"+employee);
+    }
+  },
+  state: {
+    employees:{}
+  },
+  getters:{
+    people: state=>state.employees
+  }
+});
