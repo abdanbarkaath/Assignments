317f9b9aaa2bb8ac4d23e2ee44f35bad1258ceb8
abdanbarkaath <abdanbarkaath@fissionlabs.in>
2019-11-01 11:07:59 +0530

    added react tictactoe

--- TicTacEvent/app.js
+++ TicTacEvent/app.js
@@ -9,89 +9,81 @@ var c8 = document.getElementById("col-7");
 var c9 = document.getElementById("col-8");
 var p1 = 1;
 var t1 = document.getElementById("tabl");
-var x=[1,2,3,4,5,6,7,8,9];
+var x = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-t1.addEventListener("click",function(e){
-    var text = e.target.id;
-    var charvalue = text.charAt(4)
-    console.log(charvalue); 
-          function run(){ 
-                 if (p1===1){
-                     if(e.target.textContent === "X" ||e.target.textContent===""){
-                     e.target.textContent = "X";
-                     p1 = 0;
-                 }
-                 return "X"
-                 } else {
-                     if(e.target.textContent === "O" ||e.target.textContent===""){
-                         e.target.textContent = "O";
-                         p1 = 1;
-                     }
-                 return "O"
-                 }   
-             }
-             var k = run();
-             x[charvalue]=k;
-             console.log(x);
-             setTimeout(check,500);
-})
+t1.addEventListener("click", function(e) {
+  var text = e.target.id;
+  var charvalue = text.charAt(4);
 
-function check(){
-    if(x[0]===x[1]&&x[1]===x[2]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  function run() {
+    if (p1 === 1) {
+      if (e.target.textContent === "X" || e.target.textContent === "") {
+        e.target.textContent = "X";
+        p1 = 0;
+      }
+      return "X";
+    } else {
+      if (e.target.textContent === "O" || e.target.textContent === "") {
+        e.target.textContent = "O";
+        p1 = 1;
+      }
+      return "O";
     }
-    else if(x[0]===x[3]&&x[3]===x[6]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  }
+  var k = run();
+  x[charvalue] = k;
+  console.log(x);
+  setTimeout(check, 500);
+});
+
+function check() {
+  if (x[0] === x[1] && x[1] === x[2]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
     }
-    else if(x[3]===x[4]&&x[4]===x[5]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  } else if (x[0] === x[3] && x[3] === x[6]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
     }
-    else if(x[6]===x[7]&&x[7]===x[8]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  } else if (x[3] === x[4] && x[4] === x[5]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
     }
-    else if(x[1]===x[4]&&x[4]===x[7]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  } else if (x[6] === x[7] && x[7] === x[8]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
     }
-    else if(x[2]===x[5]&&x[5]===x[8]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  } else if (x[1] === x[4] && x[4] === x[7]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
     }
-    else if(x[0]===x[4]&&x[4]===x[8]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  } else if (x[2] === x[5] && x[5] === x[8]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
     }
-    else if(x[2]===x[4]&&x[4]===x[6]){
-        if(p1==0){
-            alert("player 1 won")
-        }else{
-            alert("player 2 won")
-        }
+  } else if (x[0] === x[4] && x[4] === x[8]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
     }
+  } else if (x[2] === x[4] && x[4] === x[6]) {
+    if (p1 == 0) {
+      alert("player 1 won");
+    } else {
+      alert("player 2 won");
+    }
+  }
 }
-
